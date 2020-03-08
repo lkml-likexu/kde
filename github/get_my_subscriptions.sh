9b802508b0cc06026fd6d13d422fee357e37cb47
Aaron Qubit <like.xu@intel.com>
2020-03-08 11:26:23 +0800

    update for subscriptions.txt

--- github/get_my_subscriptions.sh
+++ github/get_my_subscriptions.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-# why 48? you need define the max value of your subscriptions,
+# why 60? you need define the max value of your subscriptions,
 # the default value is 30 entries per page
-for i in `seq 1 1 48`
+
+for i in `seq 48 1 60`
 do
-curl https://api.github.com/user/subscriptions?page=$i \
- -H "Accept: application/vnd.github.beta+json" \
--u xuliker:$username@$password >> my_subscriptions.txt
+curl "https://api.github.com/user/subscriptions?page=${i}" \
+-H "Accept: application/vnd.github.beta+json" \
+-u username:password >> "${i}_this_time_subscriptions".txt &
 done
+
+cat demo | grep -vi qemu | grep -vi kvm | grep -vi Unikernel | grep -vi Virtualization | grep -iv sgx | grep -iv Container | grep -iv Microcode | grep -iv bpf | grep -iv Kubernetes | grep -iv risc-v | grep -vi LLVM | grep -vi Android | grep -iv osx | grep -vi macos | grep -iv "Operating System" | grep -viE "Real-time|Real time" | grep -vi Compiler | grep -vi x86 | grep -vi "Virtual Machine" | grep -viE "Linux-Kernel|Linux Kernel" | grep -iv Lisp | grep -vi hypervisor | less
