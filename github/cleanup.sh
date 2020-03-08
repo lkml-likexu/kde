076123e477d7e3dee89c0de60117b2508695f551
Aaron Qubit <like.xu@intel.com>
2020-03-08 08:53:21 +0800

    add cleanup.sh for github project reviewer

---
+++ github/cleanup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -rf projects.20*
+
+cat projects_urls.20* | sort | uniq | awk -F 'github.com/' '{print $2}' >> projects.total
+
+cat projects.total | awk '{ print $1 }' | sed 's/"//g' | sort | uniq > tmp
+mv tmp projects.total
+
+rm projects_urls.20*
+
+cat users.20* | sort | uniq | awk -F 'github.com/' '{print $2}' >> users.total
+cat users.total | awk '{ print $1 }' | sed 's/"//g' | sort | uniq > tmp
+mv tmp users.total
+
+rm -rf users.20*
