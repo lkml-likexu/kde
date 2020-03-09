77d156469d59dbe1aef714d6b1a8acab148d2141
Like Xu <like.xu@linux.intel.com>
2020-03-09 15:24:14 +0000

    update get_top_contributor.sh

--- kernel_mailing_list/kvm_community/get_top_contributor.sh
+++ kernel_mailing_list/kvm_community/get_top_contributor.sh
@@ -10,4 +10,8 @@ done < target.list
 
 cat output | sort |uniq -c|sort -nr > contributors_output.txt
 
+# https://www.it610.com/article/3096642.htm
+cat output | awk -F '@' '{print $2}' | sed 's/>//g' | \
+sort| uniq -c | awk '{a[$1" "$2]+=$3}END{for (i in a){print i" "a[i]}}'|sort -nr > company_output.txt
+
 rm target.list output
