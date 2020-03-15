49b29f4dc9131b834a75cb86c706ceb0cfb9ea30
Aaron Qubit <like.xu@intel.com>
2020-03-15 10:52:27 +0800

    OS Design * An operating system designed for hosting containers https://github.com/bottlerocket-os/bottlerocket * Microkernel Hypervisor for ARMv8-A https://github.com/minosproject/minos * The kernel for the hobby operating system project, Veracyon. https://github.com/tjhancocks/vkernel * A hypervisor written in rust https://github.com/mythril-hypervisor * Xous Kernel This contains the core kernel for Xous. It requires a stage 1 loader in order to start up, as it assumes the system is already running in Supervisor mode. https://github.com/betrusted-io/xous-kernel
    Language
    * The Felix Programming Language https://github.com/felix-lang/felix
    * Open-source implementation of the K programming language https://github.com/kevinlawler/kona
    * the Yin programming language https://github.com/NewLisper/yin
    
    Programming
    * An instant messaging system supporting cross-platforms https://github.com/jeffrey-garcia/IM-Architecture-Design
    * A new basis for open, interoperable, decentralised real-time communication https://github.com/matrix-org
    * Multiplication using AVX512 and AVX512IFMA instructions https://github.com/vkrasnov/vpmadd
    * Asynchronous Rust programming, the familiar way https://github.com/async-rs
    * Chrome & Firefox Browser extension to aid gRPC-Web development https://github.com/SafetyCulture/grpc-web-devtools
    * A Control Flow Graph basic block generator for use with the libdisasm x86 disassembler, written in C. https://github.com/jwmullally/libdis_cfg
    * Apple Continuity Protocol Reverse Engineering and Dissector https://github.com/furiousMAC/continuity
    * A simple command line notebook for programmers https://github.com/dnote
    * LLVM-based Mutation Testing https://github.com/mull-project
    * Software transactional memory https://github.com/Marthog/rust-stm
    * C memory allocator based on pools, thread safe. https://github.com/philip-wernersbach/memory-pool-allocator

--- github/get_my_subscriptions.sh
+++ github/get_my_subscriptions.sh
@@ -9,4 +9,6 @@ curl "https://api.github.com/user/subscriptions?page=${i}" \
 -u username:password >> "${i}_this_time_subscriptions".txt &
 done
 
-cat demo | grep -vi qemu | grep -vi kvm | grep -vi Unikernel | grep -vi Virtualization | grep -iv sgx | grep -iv Container | grep -iv Microcode | grep -iv bpf | grep -iv Kubernetes | grep -iv risc-v | grep -vi LLVM | grep -vi Android | grep -iv osx | grep -vi macos | grep -iv "Operating System" | grep -viE "Real-time|Real time" | grep -vi Compiler | grep -vi x86 | grep -vi "Virtual Machine" | grep -viE "Linux-Kernel|Linux Kernel" | grep -iv Lisp | grep -vi hypervisor | less
+cat subscriptions.txt | grep -E "html_url|description" | grep -v "      " > demo
+
+cat demo | grep -vi qemu | grep -vi kvm | grep -vi Unikernel | grep -vi Virtualization | grep -iv sgx | grep -iv Container | grep -iv Microcode | grep -iv bpf | grep -iv Kubernetes | grep -iv risc-v | grep -vi LLVM | grep -vi Android | grep -iv osx | grep -vi macos | grep -iv "Operating System" | grep -viE "Real-time|Real time" | grep -vi Compiler | grep -vi x86 | grep -vi "Virtual Machine" | grep -viE "Linux-Kernel|Linux Kernel" | grep -iv Lisp | grep -vi hypervisor | grep -vi "Database" | grep -vi "Interview" | grep -vi "Microkernel" | grep -vi "vim" | grep -vi "Pytorch" | grep -vi "Haskell" | grep -vi "benchmark" | grep -vi "algorithm" | grep -vi " Git " | grep -vi "Cheat" | grep -vi "GPU" | grep -vi "crypto" | grep -vi "Linux\-" | grep -vi "Assembly" | grep -vi "FUSE" | grep -vi "memory" | grep -vi "Binary" | grep -iv "C/C++" | grep -iv "kernel"
