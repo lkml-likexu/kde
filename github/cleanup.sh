c22800c492c00756826c22a6adbf031e14742aa9
Aaron Qubit <like.xu@intel.com>
2020-05-09 11:29:40 +0800

    cleanup github/project.* users.*

--- github/cleanup.sh
+++ github/cleanup.sh
@@ -2,6 +2,11 @@
 
 rm -rf projects.20*
 
+# cat projects.total| awk '{print $1}' | sort | uniq > tmp
+# mv tmp projects.total
+# cat users.total | awk '{print $1}' | sort | uniq > tmp
+# mv tmp users.total
+
 cat projects_urls.20* | sort | uniq | awk -F 'github.com/' '{print $2}' >> projects.total
 
 cat projects.total | awk '{ print $1 }' | sed 's/"//g' | sort | uniq > tmp
